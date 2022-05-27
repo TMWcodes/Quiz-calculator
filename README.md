7c13b7f31c6cad7ba139fdfda2d7dfe0cc13ed65
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-05-27 16:57:45 +0100

    Able to change answers

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 # Quiz-calculator
 
-step 1 
-user selects answers 
+step 1
+user selects answers
+user can change answers by selection a different answer
 answers get averaged and user gets their result.
 
 Choice Vanilla v Jquery
 reference: https://css-tricks.com/now-ever-might-not-need-jquery/
 
 Jquery
-Jquery CDN *
+Jquery CDN \*
 var $elem =$(".someClass")
 $elem.remove();
 $elem.prepend($otherElem);
 //same with .before .replaceWith .closest
-
 
 Vanilla
 var elem = document.querySelector(".someClass")
 elem.remove()
 $elem.prepend(otherElem);
 //same with .before .replaceWith .closest
+
+formatted with prettier
+right click - format with prettier
+or
+setting - format on save + default formatter (prettier)
--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // stores the number of questions
-        var questions = document.querySelector(".questions").length; 
+var questions = document.querySelector(".questions").length;
 //    stores the sum of the answers user selected
-        var total = 0; 
-//   stores the avg of the selected answers 
-        var average = 0; 
-        
-        $('.answer').on('click', function(){
-          $(this).addClass('selected');
-          total += $(this).data('value');
-        
-          console.log(total);
-        })
+var total = 0;
+//   stores the avg of the selected answers
+var average = 0;
+
+$(".answer").on("click", function () {
+  //traverse Dom, whatever was clicked go up to the parent and find class of selected, remove its value from total
+  if ($(this).parent().find(".selected").length > 0) {
+    total -= $(this).parent().find(".selected").data("value");
+    $(this).parent().find(".selected").removeClass("selected");
+  }
+
+  $(this).addClass("selected");
+  //from data-value
+  total += $(this).data("value");
+
+  console.log(total);
+});
